e61ea4fe13a67d6620a209a0e0b0f43c59f9451c
calm1205 <naginani072@gmail.com>
2022-05-13 08:47:33 +0900

    first page

---
+++ src/Router/Router.tsx
@@ -0,0 +1,24 @@
+import React, { lazy, Suspense, useCallback } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const Router: React.FC = () => {
+  const Top = lazy(() => import("~/components/pages/Top"));
+
+  const suspensise = useCallback(
+    (Component: React.LazyExoticComponent<() => JSX.Element>) => (
+      <Suspense fallback={<>...</>}>
+        <Component />
+      </Suspense>
+    ),
+    []
+  );
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={suspensise(Top)} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Router } from "./Router/Router";
 
 const rootDOM = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootDOM);
 
 root.render(
   <React.StrictMode>
-    <p>hello world</p>
+    <Router />
   </React.StrictMode>
 );
