d9d8aa841ab89b70f887282c57eef07ba00f550f
calm1205 <naginani072@gmail.com>
2022-05-13 09:20:25 +0900

    reset cssの導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "./Router/Router";
+import { Reset } from "styled-reset";
 
 const rootDOM = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootDOM);
 
 root.render(
   <React.StrictMode>
+    <Reset />
     <Router />
   </React.StrictMode>
 );
