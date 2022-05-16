1b4fc8655e17d673aa0865317b2128483f570c10
calm1205 <naginani072@gmail.com>
2022-05-17 07:27:10 +0900

    footer

---
+++ src/components/templates/Footer.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { theme } from "~/theme";
+
+export const Footer = styled.div`
+  height: 50px;
+  background-color: ${theme.background.color.main};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
