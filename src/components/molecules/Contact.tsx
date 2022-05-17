74e024e7eb59638467776c4f74b5118a8ca1d95f
calm1205 <naginani072@gmail.com>
2022-05-18 07:21:55 +0900

    install react-hook-form

--- src/components/molecules/Contact.tsx
+++ src/components/molecules/Contact.tsx
@@ -10,12 +10,13 @@ export const Contact: React.FC = () => {
   const [content, onChangeContent] = useFormValue();
 
   const onSubmit = async () => {
-    await emailjs.send(
+    const result = await emailjs.send(
       process.env.EMAILJS_SERVICE_ID!,
       process.env.EMAILJS_TEMPLATE_ID!,
       { message: "hello world" },
       process.env.EMAILJS_PUBLIC_KEY!
     );
+    console.log(result);
   };
 
   return (
