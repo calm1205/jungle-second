16d55f18a018bfef108379b160801ee8846abd4e
calm1205 <naginani072@gmail.com>
2022-05-25 12:20:28 +0900

    recruit head

--- src/components/pages/Recruit.tsx
+++ src/components/pages/Recruit.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { MainLayout } from "~/components/templates";
+import { BackgroundImage, Box, MainTitle } from "~/components/atoms";
+import { lounge1 } from "~/public/images";
 
 const Recruit: React.FC = () => {
-  return <MainLayout>recruit</MainLayout>;
+  return (
+    <MainLayout>
+      <BackgroundImage image={lounge1} size={{ height: "220px" }}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          marginTop="70px"
+        >
+          <MainTitle text="Recruit" />
+        </Box>
+      </BackgroundImage>
+    </MainLayout>
+  );
 };
 
 export default Recruit;
