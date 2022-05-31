5878a8dbfadbd7f0f1c7e8f38eccb54179824588
calm1205 <naginani072@gmail.com>
2022-06-01 08:01:52 +0900

    image change

--- src/public/images/index.ts
+++ src/public/images/index.ts
@@ -1,7 +1,7 @@
-export { default as lounge1 } from './lounge_jungle_1.jpg';
-export { default as lounge2 } from './lounge_jungle_2.jpg';
-export { default as lounge3 } from './lounge_jungle_3.jpg';
-export { default as lounge4 } from './lounge_jungle_4.jpg';
+export { default as lounge1 } from './lounge_1.jpg';
+export { default as lounge2 } from './lounge_2.jpg';
+export { default as lounge3 } from './lounge_3.jpg';
+export { default as lounge4 } from './lounge_4.jpg';
 
 export { default as recruit1 } from './recruit_1.png';
 export { default as recruit2 } from './recruit_2.png';
