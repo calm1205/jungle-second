7eb539292c7c2273aa343777a86f80eb14c7ae76
calm1205 <naginani072@gmail.com>
2022-05-31 21:35:38 +0900

    prettier

--- src/components/molecules/ImageGallery/ImageGallery.tsx
+++ src/components/molecules/ImageGallery/ImageGallery.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { lounge1, lounge2, lounge3, lounge4 } from "~/public/images";
-import { BackgroundImage, Box } from "~/components/atoms";
-import SwipeableViews from "react-swipeable-views";
-import { theme } from "~/theme";
-import { Hrs } from "./Hrs";
+import React, { useState } from 'react';
+import { lounge1, lounge2, lounge3, lounge4 } from '~/public/images';
+import { BackgroundImage, Box } from '~/components/atoms';
+import SwipeableViews from 'react-swipeable-views';
+import { theme } from '~/theme';
+import { Hrs } from './Hrs';
 
 export const ImageGallery: React.FC = () => {
   const [index, setIndex] = useState(0);
@@ -15,28 +15,28 @@ export const ImageGallery: React.FC = () => {
         <Box paddingX={theme.space.m}>
           <BackgroundImage
             image={lounge1}
-            size={{ width: "100%", height: "230px" }}
+            size={{ width: '100%', height: '230px' }}
           />
         </Box>
 
         <Box paddingX={theme.space.m}>
           <BackgroundImage
             image={lounge2}
-            size={{ width: "100%", height: "230px" }}
+            size={{ width: '100%', height: '230px' }}
           />
         </Box>
 
         <Box paddingX={theme.space.m}>
           <BackgroundImage
             image={lounge3}
-            size={{ width: "100%", height: "230px" }}
+            size={{ width: '100%', height: '230px' }}
           />
         </Box>
 
         <Box paddingX={theme.space.m}>
           <BackgroundImage
             image={lounge4}
-            size={{ width: "100%", height: "230px" }}
+            size={{ width: '100%', height: '230px' }}
           />
         </Box>
       </SwipeableViews>
