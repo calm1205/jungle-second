c4a43a589ab518f7c0c5ff30830e4539fbb8ced7
calm1205 <naginani072@gmail.com>
2022-05-31 23:11:46 +0900

    router

---
+++ src/router/Router.tsx
@@ -0,0 +1,26 @@
+import React, { lazy, Suspense, useCallback } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const Router: React.FC = () => {
+  const Top = lazy(() => import("~/components/pages/Top"));
+  const Recruit = lazy(() => import("~/components/pages/Recruit"));
+
+  const suspensise = useCallback(
+    (Component: React.LazyExoticComponent<React.FC<{}>>) => (
+      <Suspense fallback={<>...</>}>
+        <Component />
+      </Suspense>
+    ),
+    []
+  );
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={suspensise(Top)} />
+        <Route path="recruit" element={suspensise(Recruit)} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
