e69f1da28ae1de0ababb992ededa9c145026dfaa
calm1205 <naginani072@gmail.com>
2022-06-07 07:19:28 +0900

    conceptからタイトルを削除

--- src/components/organisms/Concept.tsx
+++ src/components/organisms/Concept.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { theme } from '~/theme';
-import { Box, Id, Paragraph, Title } from '~/components/atoms';
+import { Box, Id, Paragraph } from '~/components/atoms';
 import { ImageGallery } from '~/components/molecules';
 import { Slider } from './Slider/Slider';
 
@@ -12,16 +12,11 @@ export const Concept: React.FC = () => {
       <Box paddingY={theme.space.xl} textAlign="center">
         <Slider />
 
-        <Paragraph text="日本 東京" marginTop={theme.space.xl} />
-
-        <Box marginY={theme.space.m}>
-          <Title text="Lounge Jungle" />
-        </Box>
-
         <Paragraph
           text={
             '気品と優美さを組み込んだ洗礼された空間\n高級ホテルに居るかのような店内は\n高級感もありながら落ち着いた雰囲気で\n特別なひとときをお過ごし頂けます。\nぜひワンランク上のラグジュアリーな空間を\nお楽しみください。'
           }
+          marginTop={theme.space.l}
         />
 
         <Box marginTop={theme.space.l}>
