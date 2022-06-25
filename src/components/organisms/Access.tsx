26de37882b422a4d57616d8d517b296352f00a18
calm1205 <naginani072@gmail.com>
2022-06-25 21:52:57 +0900

    住所をリンクに変更

--- src/components/organisms/Access.tsx
+++ src/components/organisms/Access.tsx
@@ -9,6 +9,7 @@ import {
   Title,
 } from '~/components/atoms';
 import { map } from '~/public/images';
+import styled from 'styled-components';
 
 export const Access: React.FC = () => {
   return (
@@ -21,8 +22,18 @@ export const Access: React.FC = () => {
       <Box padding={theme.space.m} paddingBottom={theme.space.xl}>
         <BackgroundImage image={map} size={{ height: '260px' }} />
         <Paragraph text="六本木駅から徒歩5分" />
-        <Paragraph text="東京都港区六本木3-15-24 belle六本木1階" marginTop="" />
+
+        <A href="https://goo.gl/maps/HsgJLaCXtUbSrJQw9">
+          <Paragraph
+            text="東京都港区六本木3-15-24 belle六本木1階"
+            marginTop=""
+          />
+        </A>
       </Box>
     </BackgroundWhite>
   );
 };
+
+const A = styled.a`
+  text-decoration: underline;
+`;
