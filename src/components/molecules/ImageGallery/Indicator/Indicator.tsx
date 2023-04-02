f3cdae8f46b0463457e015ad0b94dbb10ddb8868
calm1205 <naginani072@gmail.com>
2023-04-02 19:33:18 +0900

    ImageGallery

---
+++ src/components/molecules/ImageGallery/Indicator/Indicator.tsx
@@ -0,0 +1,22 @@
+import { Box } from '~/components/atoms';
+import { FC } from 'react';
+import { Active, InActive } from './Indicator.style';
+
+type Indicator = {
+  /** インジケーターの数 */
+  length: number;
+  /** 選択されている要素index */
+  activeIndex: number;
+};
+
+/**
+ * ImageGalleryの選択中indexを示すインジケーター
+ */
+export const Indicator: FC<Indicator> = ({ length, activeIndex }) => (
+  <Box display="flex">
+    {[...Array(length)].map((_, i) => {
+      if (i === activeIndex) return <Active key={`hr_${i}`} />;
+      return <InActive key={`hr_${i}`} />;
+    })}
+  </Box>
+);
