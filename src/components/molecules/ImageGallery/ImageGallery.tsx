dba809afb2a4e043b61a7a4992cef5c4df6c9dd0
Moriyama Nagi <naginani072@gmail.com>
2023-04-02 19:34:37 +0900

    Reactのversion up (#17)
    - React18にアップデート
    - React-swipeable-viewを削除

--- src/components/molecules/ImageGallery/ImageGallery.tsx
+++ src/components/molecules/ImageGallery/ImageGallery.tsx
@@ -1,30 +1,63 @@
-import { useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { lounge1, lounge2, lounge3, lounge4 } from '~/public/images';
 import { BackgroundImage, Box } from '~/components/atoms';
-import SwipeableViews from 'react-swipeable-views';
 import { theme } from '~/theme';
-import { Hrs } from './Hrs';
+import { Indicator } from './Indicator/Indicator';
 
-export const ImageGallery: React.FC = () => {
-  const [index, setIndex] = useState(0);
-  const onChangeIndex = (i: number) => setIndex(i);
-  const images = [lounge1, lounge2, lounge3, lounge4];
+const IMAGES = [lounge1, lounge2, lounge3, lounge4];
+
+/**
+ * カルーセルの画像表示
+ */
+export const ImageGallery: FC = () => {
+  const ImageFlameRef = useRef<HTMLDivElement>(null);
+  const ImageRefs = IMAGES.map(() => useRef<HTMLDivElement>(null));
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const imageIndex = Number(entry.target.getAttribute('data-index'));
+        if (entry.isIntersecting) setActiveIndex(imageIndex);
+      });
+    },
+    { root: ImageFlameRef.current, threshold: 1 }
+  );
+
+  useEffect(() => {
+    ImageRefs.forEach((imageRef) => {
+      if (!imageRef.current) return;
+      observer.observe(imageRef.current);
+    });
+  }, []);
 
   return (
     <>
-      <SwipeableViews index={index} onChangeIndex={onChangeIndex}>
-        {images.map((image, index) => (
-          <Box paddingX={theme.space.m} key={`image_${index}`}>
+      <div
+        ref={ImageFlameRef}
+        style={{
+          display: 'flex',
+          overflowX: 'scroll',
+          scrollSnapType: 'x mandatory',
+        }}
+      >
+        {ImageRefs.map((ref, index) => (
+          <div
+            ref={ref}
+            key={`image_${index}`}
+            data-index={index}
+            style={{ minWidth: '100vw', scrollSnapAlign: 'start' }}
+          >
             <BackgroundImage
-              image={image}
+              image={IMAGES[index]}
               size={{ width: '100%', height: '230px' }}
             />
-          </Box>
+          </div>
         ))}
-      </SwipeableViews>
+      </div>
 
       <Box margin={theme.space.m}>
-        <Hrs length={4} active={index} />
+        <Indicator length={4} activeIndex={activeIndex} />
       </Box>
     </>
   );
