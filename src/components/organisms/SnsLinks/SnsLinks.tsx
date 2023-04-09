0e01bc1451478fed8fb5a73e7355b474b6d34333
calm1205 <naginani072@gmail.com>
2023-04-09 11:33:29 +0900

    SnsLinks

---
+++ src/components/organisms/SnsLinks/SnsLinks.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { Box, Instagram, Twitter } from '~/components/atoms';
+
+/**
+ * TwitterやInstagramのリンク
+ */
+export const SnsLinks: FC = () => {
+  const sns = [
+    { link: 'https://twitter.com/LoungeJUNGLE_', icon: <Twitter /> },
+    { link: 'https://www.instagram.com/loungejungle_/', icon: <Instagram /> },
+  ] as const;
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      width="100px"
+    >
+      {sns.map((s) => (
+        <a key={s.link} href={s.link} target="_blank" rel="noreferrer">
+          {s.icon}
+        </a>
+      ))}
+    </Box>
+  );
+};
