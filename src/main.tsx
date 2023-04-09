6f3cfb9c28e58aec01757cbd8a4599a7d7a58870
Moriyama Nagi <naginani072@gmail.com>
2023-04-09 10:50:26 +0900

    Feature/set react18 root (#19)

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import ResetCss from './ResetCSS';
 import { Router } from './router/Router';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import { StrictMode } from 'react';
 
-const rootDOM = document.getElementById('root');
-
-ReactDOM.render(
+const rootDom = document.getElementById('root');
+const root = rootDom && createRoot(rootDom);
+root?.render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <ResetCss />
       <Router />
     </ThemeProvider>
-  </StrictMode>,
-  rootDOM
+  </StrictMode>
 );
