bf58bf7cba6733b67ef173c39267b39ce8298cf0
calm1205 <naginani072@gmail.com>
2023-09-03 18:27:30 +0900

    remove return

--- src/components/organisms/Concept.tsx
+++ src/components/organisms/Concept.tsx
@@ -3,25 +3,23 @@ import { Box, Id, Paragraph } from '~/components/atoms';
 import { ImageGallery } from '~/components/molecules';
 import { Slider } from './Slider/Slider';
 
-export const Concept: React.FC = () => {
-  return (
-    <>
-      <Id id="home" />
+export const Concept: React.FC = () => (
+  <>
+    <Id id="home" />
 
-      <Box paddingY={theme.space.xl} textAlign="center">
-        <Slider />
+    <Box paddingY={theme.space.xl} textAlign="center">
+      <Slider />
 
-        <Paragraph
-          text={
-            '気品と優美さを組み込んだ洗礼された空間\n高級ホテルに居るかのような店内は\n高級感もありながら落ち着いた雰囲気で\n特別なひとときをお過ごし頂けます。\nぜひワンランク上のラグジュアリーな空間を\nお楽しみください。'
-          }
-          marginTop={theme.space.l}
-        />
+      <Paragraph
+        text={
+          '気品と優美さを組み込んだ洗礼された空間\n高級ホテルに居るかのような店内は\n高級感もありながら落ち着いた雰囲気で\n特別なひとときをお過ごし頂けます。\nぜひワンランク上のラグジュアリーな空間を\nお楽しみください。'
+        }
+        marginTop={theme.space.l}
+      />
 
-        <Box marginTop={theme.space.l}>
-          <ImageGallery />
-        </Box>
+      <Box marginTop={theme.space.l}>
+        <ImageGallery />
       </Box>
-    </>
-  );
-};
+    </Box>
+  </>
+);
