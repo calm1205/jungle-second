c0ca4ace930f1baeedac6cd49a9de121f4ff99bf
calm1205 <naginani072@gmail.com>
2023-09-23 21:07:45 +0900

    Member削除

--- src/components/molecules/Fee/FeeSection.tsx
+++ src/components/molecules/Fee/FeeSection.tsx
@@ -1,24 +1,22 @@
 import { Box, SubTitle } from '~/components/atoms'
 import { FeeLi, FeeUl } from '~/components/molecules'
 
-type FeeSection = {
-  title: string;
-  fees: FeeLi[];
-};
+export type FeeSection = {
+  title: string
+  fees: FeeLi[]
+}
 
 /**
  * 料金の1セクション
  */
-export const FeeSection: React.FC<FeeSection> = ({ title, fees }) => {
-  return (
-    <Box display="flex" justifyContent="space-between" alignItems="center">
-      <Box width="40%" textAlign="center">
-        <SubTitle text={title} />
-      </Box>
-
-      <Box width="45%">
-        <FeeUl lists={fees} />
-      </Box>
+export const FeeSection: React.FC<FeeSection> = ({ title, fees }) => (
+  <Box display="flex" justifyContent="space-between" alignItems="center">
+    <Box width="40%" textAlign="center">
+      <SubTitle text={title} />
     </Box>
-  )
-}
+
+    <Box width="45%">
+      <FeeUl lists={fees} />
+    </Box>
+  </Box>
+)
--- src/components/organisms/System.tsx
+++ src/components/organisms/System.tsx
@@ -9,90 +9,77 @@ import {
 import { theme } from '~/theme'
 import { FeeUl, FeeSection } from '~/components/molecules'
 
-export const System: React.FC = () => {
-  return (
-    <BackgroundWhite>
-      <Id id="system" />
-      <Box paddingY={theme.space.xl} textAlign="center">
-        <Title text="System" />
-      </Box>
-
-      <Box paddingX={theme.space.m}>
-        <Hr />
-
-        <Box margin={`${theme.space.l} ${theme.space.m}`}>
-          <FeeSection
-            title="Regular"
-            fees={[
-              { name: '1set', value: '¥15,000' },
-              { name: 'additional', value: '¥8,000' },
-            ]}
-          />
-        </Box>
-
-        <Hr />
-
-        <Box margin={`${theme.space.l} ${theme.space.m}`}>
-          <FeeSection
-            title="Member"
-            fees={[
-              { name: '1set', value: '¥12,000' },
-              { name: 'additional', value: '¥6,000' },
-            ]}
-          />
-        </Box>
-
-        <Hr />
-
-        <Box margin={`${theme.space.l} ${theme.space.m}`}>
-          <FeeSection
-            title="Vip"
-            fees={[
-              { name: '1set', value: '¥18,000' },
-              { name: 'charge', value: '¥0' },
-              { name: 'additional', value: '¥9,000' },
-            ]}
-          />
-        </Box>
-
-        <Hr />
-
-        <Box marginTop={theme.space.m} paddingX={theme.space.xl}>
-          <FeeUl
-            lists={[
-              { name: '1set', value: '60min' },
-              { name: 'charge', value: '60min' },
-              { name: 'additional', value: '30min' },
-            ]}
-          />
-
-          <Box marginTop={theme.space.m}>
-            <FeeUl
-              lists={[
-                { name: '本指名(60min)', value: '¥3,000' },
-                { name: '場内指名(60min)', value: '¥5,000' },
-                { name: '同伴', value: '¥5,000' },
-              ]}
-            />
-          </Box>
-
-          <Box marginTop={theme.space.m}>
-            <FeeUl
-              lists={[
-                { name: 'サービス料', value: '30%' },
-                { name: '消費税', value: '10%' },
-              ]}
-            />
-          </Box>
-        </Box>
-        <Footnote
-          text={
-            '\n※ 自動延長制となります。\n※ 追加指名は1人につき、セット料金の50%頂きます。\n※ オーダー料金は別途頂戴いたします。\n※ 会員登録時にお名刺を頂戴いたします。\n※ 同業者、スカウトマンの入店はお断りいたします。'
-          }
-        />
-      </Box>
-
-      <Box paddingBottom={theme.space.xl} />
-    </BackgroundWhite>
-  )
+type PriceType = 'regular' | 'vip' | 'basic' | 'designate' | 'service'
+type PriceSystems = {
+  [key in PriceType]: FeeSection['fees']
 }
+
+const PRICE_SYSTEMS: PriceSystems = {
+  regular: [
+    { name: '1set', value: '¥15,000' },
+    { name: 'additional', value: '¥8,000' },
+  ],
+  vip: [
+    { name: '1set', value: '¥18,000' },
+    { name: 'charge', value: '¥0' },
+    { name: 'additional', value: '¥9,000' },
+  ],
+  basic: [
+    { name: '1set', value: '60min' },
+    { name: 'charge', value: '60min' },
+    { name: 'additional', value: '30min' },
+  ],
+  designate: [
+    { name: '本指名(60min)', value: '¥3,000' },
+    { name: '場内指名(60min)', value: '¥5,000' },
+    { name: '同伴', value: '¥5,000' },
+  ],
+  service: [
+    { name: 'サービス料', value: '30%' },
+    { name: '消費税', value: '10%' },
+  ],
+}
+
+export const System: React.FC = () => (
+  <BackgroundWhite>
+    <Id id="system" />
+    <Box paddingY={theme.space.xl} textAlign="center">
+      <Title text="System" />
+    </Box>
+
+    <Box paddingX={theme.space.m}>
+      <Hr />
+
+      <Box margin={`${theme.space.l} ${theme.space.m}`}>
+        <FeeSection title="Regular" fees={PRICE_SYSTEMS.regular} />
+      </Box>
+
+      <Hr />
+
+      <Box margin={`${theme.space.l} ${theme.space.m}`}>
+        <FeeSection title="Vip" fees={PRICE_SYSTEMS.vip} />
+      </Box>
+
+      <Hr />
+
+      <Box marginTop={theme.space.m} paddingX={theme.space.xl}>
+        <FeeUl lists={PRICE_SYSTEMS.basic} />
+
+        <Box marginTop={theme.space.m}>
+          <FeeUl lists={PRICE_SYSTEMS.designate} />
+        </Box>
+
+        <Box marginTop={theme.space.m}>
+          <FeeUl lists={PRICE_SYSTEMS.service} />
+        </Box>
+      </Box>
+      <Footnote
+        text={
+          '\n※ 自動延長制となります。\n※ 追加指名は1人につき、セット料金の50%頂きます。\n※ オーダー料金は別途頂戴いたします。\n※ 会員登録時にお名刺を頂戴いたします。\n※ 同業者、スカウトマンの入店はお断りいたします。'
+        }
+      />
+    </Box>
+
+    <Box paddingBottom={theme.space.xl} />
+  </BackgroundWhite>
+)
